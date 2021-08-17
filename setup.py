75a8d03cc34eea30bd475f2470e6b4fc917e0ab7
karthi12ck <42664722+karthi12ck@users.noreply.github.com>
2021-08-17 04:16:29 -0400

    update code

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'Model_Explaination',         # How you named your package folder (MyLib)
+  name = 'Model_Explainationn',         # How you named your package folder (MyLib)
   packages = ['Model_Explaination'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
